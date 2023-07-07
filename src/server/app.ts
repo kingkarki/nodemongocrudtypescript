12046d4264ce81e0ddeb05f5b722537aa0490f88
Krishna Karki <developersnepal@gmail.com>
2023-07-07 13:09:44 +0545

    cors added

--- src/server/app.ts
+++ src/server/app.ts
@@ -1,22 +1,26 @@
+import express from "express";
+import { db } from "../Config/db.config";
+import { router } from "../Routes/posts.routes";
+const cors = require("cors");
 
-import express from 'express'
-import { db} from '../Config/db.config'
-import { router } from '../Routes/posts.routes'
-
-const app = express()
+const app = express();
 
 //middlewares
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+//cors
+
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 
 //routes
-app.use('/api/v1/posts', router)
+app.use("/api/v1/posts", router);
 
 //db connection then server connection
 db.then(() => {
-    app.listen(7070, () => console.log('Server is listening on port 7070'))
-})
-
-
-
+  app.listen(7070, () => console.log("Server is listening on port 7070"));
+});
